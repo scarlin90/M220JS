0fd5bdb1f1b9d175dc83492fc3f57499e08f84cb
Sean Carlin <scarlin@smashfly.com>
2019-03-03 20:01:12 +0000

    Adding wtimeouts and conection pooling

--- Week3/src/index.js
+++ Week3/src/index.js
@@ -26,7 +26,11 @@ MongoClient.connect(
   // Set the poolSize to 50 connections.
   // TODO: Timeouts
   // Set the write timeout limit to 2500 milliseconds.
-  { useNewUrlParser: true },
+  {
+    poolSize: 50,
+    wtimeout: 2500,
+    useNewUrlParser: true,
+  },
 )
   .catch(err => {
     console.error(err.stack)
--- Week3/test/config/mongoEnvironment.js
+++ Week3/test/config/mongoEnvironment.js
@@ -9,7 +9,11 @@ module.exports = class MongoEnvironment extends NodeEnvironment {
         // Set the connection pool size to 50 for the testing environment.
         // TODO: Timeouts
         // Set the write timeout limit to 2500 milliseconds for the testing environment.
-        { useNewUrlParser: true }
+        {
+          poolSize: 50,
+          wtimeout: 2500,
+          useNewUrlParser: true,
+        },
       )
       await super.setup()
     }
