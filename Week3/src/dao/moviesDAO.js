d76423ea49ce705829feb75ac639eeaa9fdb1c8f
Sean Carlin <scarlin@smashfly.com>
2019-03-03 21:34:25 +0000

    return null in catch

--- Week3/src/dao/moviesDAO.js
+++ Week3/src/dao/moviesDAO.js
@@ -348,7 +348,10 @@ export default class MoviesDAO {
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
       console.error(`Something went wrong in getMovieByID: ${e}`)
-      throw e
+
+      console.error(`e log: ${e.toString()}`)
+
+      return null
     }
   }
 }
